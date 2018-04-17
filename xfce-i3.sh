7089fb891a34bc96f0a7325865b681abd5efdf5e
Caio Freitas de Oliveira <caio@home-one>
2018-04-17 07:47:46 -0300

    add .sh & +x

--- xfce-i3.sh
+++ xfce-i3.sh
@@ -5,3 +5,4 @@ CONFIGDIR=/home/caio/Documents/desktop-config
 i3 -c $CONFIGDIR/$1/i3.config
 i3-msg exec feh --bg-fill $CONFIGDIR/$1/background.png
 i3-msg exec conky -d -c $CONFIGDIR/$1/conky.config
+i3-msg exec xset dpms 120 120 300
