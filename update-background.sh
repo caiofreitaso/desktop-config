c760d056eb3105c0503f0c121369cb2cc65a6937
Caio Freitas de Oliveira <caio@home-one>
2018-04-18 10:34:16 -0300

    add rotation script

--- update-background.sh
+++ update-background.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PROFILE=$1
+ROTATE=$2
+
+xrandr --output HDMI1 --rotate $ROTATE
 
 ps -aux | awk '{if($11 ~ /conky/){print $2}}' | xargs -n1 kill
 conky -d -c ~/.i3/$PROFILE/conky.config
