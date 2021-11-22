8bbe2a81a5ea6e581e063de925a4a1016545d39d
Caio Freitas de Oliveira <caiofreitaso@gmail.com>
2021-11-22 11:35:24 -0300

    use media keys; add update-monitor script

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,44 +1,67 @@
 #!/bin/bash -ev
 
-# Debian repositories
+APTITUDE_PACKAGES=(
+  curl
+  i3
+  gimp
+  inkscape
+  darktable
+  remmina
+  thunderbird
+)
+DPKG_DEB=( )
+REQUIREMENTS=( )
+
+add_sublime() {
+  echo deb https://download.sublimetext.com/ apt/stable/ | tee /etc/apt/sources.list.d/sublime-text.list
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+  REQUIREMENTS+=( apt-transport-https )
+  APTITUDE_PACKAGES+=( sublime-text )
+}
+add_opera() {
+  echo deb https://deb.opera.com/opera-stable/ stable non-free | tee /etc/apt/sources.list.d/opera.list
+  wget -qO- https://deb.opera.com/archive.key | apt-key add -
+  APTITUDE_PACKAGES+=( opera-stable )
+}
+add_spotify() {
+  echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+  APTITUDE_PACKAGES+=( spotify-client )
+}
+download_steam() {
+  STEAM_URL="http://media.steampowered.com/client/installer/steam.deb"
+  echo "Downloading steam.deb"
+  wget -q $STEAM_URL
+  DPKG_DEB+=( steam.deb )
+}
+download_cockatrice() {
+  AWK_SCRIPT="
+  {
+    if ($1 ~ /browser_download/ && $2 ~ /\.deb/) {
+      gsub(/\"/, "", $2);
+      print $2;
+    }
+  }"
+  COCKATRICE_URL=$(\
+    curl -s https://api.github.com/repos/Cockatrice/Cockatrice/releases/latest \
+    | awk "${AWK_SCRIPT}" \
+  )
+  COCKATRICE_FILE=$(echo ${COCKATRICE_URL} | rev | awk -F/ '{print $1}' | rev)
+
+  echo "Downloading $COCKATRICE_FILE"
+  wget -q $COCKATRICE_URL
+  DPKG_DEB+=( "${COCKATRICE_FILE}" )
+}
+
+download_steam
+download_cockatrice
+dpkg -i ${DPKG_DEB[@]}
+
+add_opera
+add_sublime
+add_spotify
 aptitude update
-aptitude install i3 gimp inkscape darktable remmina thunderbird
+aptitude install ${REQUIREMENTS[@]}
+aptitude install ${APTITUDE_PACKAGES[@]}
 
-# Sublime-Text
-## 1) Add GPG key
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-## 2) Ensure HTTPS works
-aptitude install apt-transport-https
-## 3) Add to source list
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-
-# Opera
-## 1) Add repository
-echo 'deb https://deb.opera.com/opera-stable/ stable non-free' | tee /etc/apt/sources.list.d/opera.list
-## 2) Add GPG key
-wget -qO- https://deb.opera.com/archive.key | apt-key add -
-
-# Spotify
-## 1) Add the Spotify repository signing keys to be able to verify downloaded packages
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-## 2) Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
-
-# Steam
-STEAM_URL="http://media.steampowered.com/client/installer/steam.deb"
-echo "Downloading steam.deb"
-wget -q $STEAM_URL
-
-# Cockatrice
-COCKATRICE_URL=$(curl -s https://api.github.com/repos/Cockatrice/Cockatrice/releases/latest |\
- awk '{if ($1 ~ /browser_download/ && $2 ~ /\.deb/) {gsub(/\"/, "", $2); print $2;}}')
-COCKATRICE_FILE=$(echo ${COCKATRICE_URL} | rev | awk -F/ '{print $1}' | rev)
-echo "Downloading $COCKATRICE_FILE"
-wget -q $COCKATRICE_URL
-
-dpkg -i steam.deb $COCKATRICE_FILE
-
-aptitude update
-aptitude install opera-stable sublime-text spotify-client
-
-rm steam.deb $COCKATRICE_FILE
+rm ${DPKG_DEB_[@}}
--- xfce-i3.sh
+++ xfce-i3.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 PROFILE=$1
-TARGET_DISPLAY=${2:-:0.0}
-DISPLAY=${TARGET_DISPLAY}
+TARGET_CONNECTOR=${2:-DP}
+DISPLAY=${3:-:0.0}
 CONFIGDIR=/home/caio/Documents/desktop-config
 
 rm -rf ~/.i3/*
 cp -R $CONFIGDIR/$PROFILE ~/.i3/
 
+if [[ "${PROFILE}" =~ -v$ ]]
+then
+  $CONFIGDIR/update-monitor.sh ${TARGET_CONNECTOR}
+fi
 i3 -c ~/.i3/$PROFILE/i3.config
